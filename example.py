976e6c9344de90794c964bd0bdd673b5ff00ce6f
TheShadowNinjaYT <77973105+TheShadowNinjaYT@users.noreply.github.com>
2021-04-23 16:23:39 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,19 @@
+#this is the example file which demonstrates some basic functionality
+
 import encryptoSave
+import random
 
 p = encryptoSave.player()
+
+#IF YOU PUBLICALLY RELEASE YOUR CODE, DO NOT USE THIS FUNCTION HERE! FIND ANOTHER WAY
+encryptoSave.setKey("example key")
+
+encryptoSave.addVar(p, "xp", random.randint(1,1000))
+
+print(p.variables)
 
 encryptoSave.save(p)
 
 encryptoSave.load(p)
 
-print(p.xp)
+print(p.variables["xp"])
