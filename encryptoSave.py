061391fbf46eda6d9dad6d41d48be0dd9c768c5d
TheShadowNinjaYT <77973105+TheShadowNinjaYT@users.noreply.github.com>
2021-04-23 16:21:24 +0100

    added new functions

--- encryptoSave.py
+++ encryptoSave.py
@@ -9,9 +9,20 @@ import sys
 """
 Here you need to make a .env file and write (export encryptkey1="{random letters and numbers here}")
 """
-encryptkey1 = os.environ['encryptkey1']
-encryptkey2 = os.environ['encryptkey1']
 
+key1 = ""
+
+#sets the key from another file
+def setKey(key):
+
+  global key1
+
+  key1 = key
+
+encryptkey1 = key1
+encryptkey2 = key1
+
+#it is not neccessary to call these! they are already at work
 def encrypt(data):
   auths=URLSafeSerializer(encryptkey1,encryptkey2)
   return auths.dumps(data)
@@ -27,22 +38,21 @@ class player():
   def __init__(self):
 
      #example variables
-     self.xp = 198
-     self.damage = 30
-     self.health = 100
+     self.variables = {
+       
+     }
 
 #you can make your own player object by calling encryptoSave.player()
 p = player()
 
+def addVar(player, varname, value):
+
+  player.variables[varname] = value
+
 #call encryptoSave.save(p) to save the player's state
 def save(player):
 
-  data = {
-    #put all variables you wish to save here
-    "xp": player.xp,
-    "damage": player.damage,
-    "health": player.health
-  }
+  data = player.variables
 
   with open("save.txt", "w") as f:
       f.write(encrypt(data))
@@ -60,10 +70,6 @@ def load(player):
             #here you need to retrieve any variables you have saved
 
             player.xp = savedData['xp']
-
-            player.health = savedData['health']
-
-            player.damage = savedData['damage']
 
   else:
 
